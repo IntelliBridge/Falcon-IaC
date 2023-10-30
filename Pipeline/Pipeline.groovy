f4691604ae04c75db58b92ad828a0155d6023d01
asedrak1 <149423607+asedrak1@users.noreply.github.com>
2023-10-30 16:18:39 -0400

    Create Pipeline.groovy

---
+++ Pipeline/Pipeline.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git "https://github.com/aws-samples/api-gateway-secure-pet-store.git"
+
+                // Run Maven
+                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+            }}
+
+            stage('Sonarqube Scan'){
+                steps {
+                    sh '''/tools/sonarscanner/bin/sonar-scanner \
+                        -Dsonar.projectKey=pet-store \
+                        -Dsonar.projectName=pet-store \
+                        -Dsonar.sources=src/main/java/ \
+                        -Dsonar.language=java \
+                        -Dsonar.host.url=http://localhost:9000 \
+                        -Dsonar.login=admin \
+                        -Dsonar.java.binaries=**/**/**/**/**/**/**  \
+                        -Dsonar.password=MySonarAdminPassword \
+                        -Dsonar.sslVerification=false
+                    '''
+                }
+            }
+
+        }
+    }
